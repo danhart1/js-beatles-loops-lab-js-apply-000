508ca7ab4ddabcf549b63269c94b7bc778186403
danhart1 <danhart249@live.com>
2018-09-03 15:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function johnLennonFacts(facts){
     var iLuvBtls =[];
       do{
      iLuvBtls.push('I love the Beatles!');
-     return iLuvBtls;
+    return iLuvBtls;
    }while(numba<15);
   }
   
